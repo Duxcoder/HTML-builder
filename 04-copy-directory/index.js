2cbd76e9f3825da5639722933a98e548804bb71f
Duxcoder <iinegve_25@mail.ru>
2023-05-03 22:21:58 +0300

    feat: complete task4

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,23 @@
+const fs = require('fs').promises;
+const path = require('path');
 
+async function copyDir(copyDir, nameNewDir) {
+  const { readdir } = fs;
+  const pathCopyDir = path.join(__dirname, copyDir);
+  const pathNewDir = path.join(__dirname, nameNewDir);
+  try {
+    await fs.rm(pathNewDir, { recursive: true, force: true});
+    await fs.mkdir(pathNewDir, { recursive: true });
+    const files = await readdir(pathCopyDir);
+    files.forEach(file => {
+      const pathFile = (pathDir) => path.join(pathDir, file)
+      fs.copyFile(pathFile(pathCopyDir), pathFile(pathNewDir));
+    });
+    console.log(`${copyDir} copied to ${nameNewDir}`)
+  }
+  catch (error) {
+    console.log(error);
+  }
+}
+
+copyDir('files', 'files-copy');
