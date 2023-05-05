b23ec8e9c3212ec58bf894e72051426f6d73c1e7
Duxcoder <iinegve_25@mail.ru>
2023-05-05 20:43:00 +0300

    feat: complete task6

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -1,35 +1,38 @@
-const fs = require('fs/promises');
+const fs = require('fs');
+const fsp = require('fs/promises')
 const path = require('path');
 
-async function createDir(path) {
-  await fs.mkdir(path, { recursive: true })
-}
 const pathProject = path.join(__dirname, 'project-dist');
-createDir(pathProject);
-
 const pathComponents = path.join(__dirname, 'components');
 const pathTemplate = path.join(__dirname, 'template.html');
 const pathIndex = path.join(pathProject, 'index.html');
+const pathDirStyles = path.join(__dirname, 'styles');
+const pathStyle = path.join(pathProject, 'style.css');
+const pathAssets = path.join(__dirname, 'assets');
+const pathCopyAssets = path.join(pathProject, 'assets');
 
+const createDir = function (path) {
+  return fsp.mkdir(path, { recursive: true })
+}
 
 const getComponents = function () {
-  return fs.readdir(pathComponents)
+  return fsp.readdir(pathComponents)
     .then(files => {
       let components = files.map(file => {
         const name = file.replace('.html', '');
         const pathFile = path.join(pathComponents, file)
-        return fs.readFile(pathFile)
+        return fsp.readFile(pathFile)
           .then((html) => {
             return [name, html.toString()]
           })
       })
-      return Promise.all(components)
+      return Promise.all(components);
     })
 }
 
 const insertComponents = function (path) {
   const components = getComponents();
-  return fs.readFile(path)
+  return fsp.readFile(path)
     .then((htmlCode) => {
       return htmlCode.toString()
     })
@@ -49,11 +52,59 @@ const insertComponents = function (path) {
 const createIndex = function (path) {
   const html = insertComponents(pathTemplate);
   return html.then((res) => {
-    return fs.writeFile(path, res)
+    return fsp.writeFile(path, res);
   })
 }
 
-// const createBundleCss = function (path) {
+const createBundle = async function (pathDir, pathBundle) {
+  try {
+    const files = await fsp.readdir(pathDir);
+    const cssFiles = async function () {
+      let data = '';
+      for (const file of files) {
+        if (path.extname(file) == ".css") {
+          const pathFile = path.join(pathDir, file);
+          const readableStream = fs.createReadStream(pathFile, 'utf-8');
+          for await (const chunk of readableStream) {
+            data += chunk;
+          }
+        };
+      }
+      return data;
+    }
+    const cssText = await cssFiles();
+    await fsp.writeFile(pathBundle, cssText);
+  }
+  catch (error) {
+    console.log(error);
+  }
+}
 
-// }
-createIndex(pathIndex);
+async function copyDir(pathCopyDir, pathNewDir) {
+  const { readdir, lstat } = fsp;
+  try {
+    await fsp.rm(pathNewDir, { recursive: true, force: true });
+    await fsp.mkdir(pathNewDir, { recursive: true });
+    const files = await readdir(pathCopyDir);
+    for (let file of files) {
+      const sourcePath = path.join(pathCopyDir, file);
+      const distPath = path.join(pathNewDir, file);
+      const stats = await lstat(sourcePath);
+      if (stats.isDirectory()) {
+        await copyDir(sourcePath, distPath);
+      } else {
+        await fsp.copyFile(sourcePath, distPath);
+      }
+    }
+  }
+  catch (error) {
+    console.log(error);
+  }
+}
+
+createDir(pathProject)
+  .then(() => {
+    createIndex(pathIndex);
+    createBundle(pathDirStyles, pathStyle);
+    copyDir(pathAssets, pathCopyAssets);
+  });
