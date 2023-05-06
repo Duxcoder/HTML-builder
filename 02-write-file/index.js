737367a9d5da202b37e5ccc335906a3c4b728af9
Duxcoder <iinegve_25@mail.ru>
2023-05-06 23:55:16 +0300

    fix: fix line break for other os

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -1,26 +1,31 @@
 const fs = require('fs');
 const path = require('path');
 const process = require('process');
+const os = require('os');
 
 const { stdin, stdout } = process;
 const url = path.join(__dirname, 'text.txt')
 
 fs.writeFile(url, '', (err) => {
   if (err) throw err;
-  console.log('Хай, как ты сюда забрёл друг? Ну-ка расскажи мне в консоли...');
+  console.log('Хай, как ты сюда забрёл друг? Ну-ка расскажи мне в консоли...' + os.EOL);
 });
 
 process.on('exit', () => {
-  stdout.write('Да? Очень интересно, ладно, всё удачи, мне пора!');
+  stdout.write('Да? Очень интересно, ладно, всё удачи, мне пора!' + os.EOL);
   process.exit();
 });
 
-stdin.on('data', data => fs.appendFile(url, data, err => {
-  if (err) throw err;
-  if (data.toString().trim() === 'exit') process.exit();
-}));
+stdin.on('data', data => {
+  const isExit = (data) => data.toString().trim() === 'exit';
+  const message = isExit(data) ? '' : data.toString().replace(os.EOL, '') + os.EOL;
+  fs.appendFile(url, message, err => {
+    if (err) throw err;
+    if (isExit(data)) process.exit();
+  })
+});
 
 process.on('SIGINT', () => {
-  console.log('');
+  console.log(os.EOL);
   process.exit()
 });
