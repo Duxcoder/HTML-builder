a7985b4a3bf75c877243d1d4035d363163e6db8c
Duxcoder <iinegve_25@mail.ru>
2023-05-07 21:08:50 +0300

    fix: using stdout instead of console.log

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const path = require('path');
-const url = path.join(__dirname + '/text.txt')
+const url = path.join(__dirname + '/text.txt');
 const readableStream = fs.createReadStream(url, 'utf-8');
-readableStream.on('data', text => console.log(text));
+const { stdout } = process;
+readableStream.on('data', text => stdout.write(text));
